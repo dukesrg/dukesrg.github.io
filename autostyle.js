bd611c5947a856bd32d181560ae802758d5ebc34
dukesrg <dukesrg@gmail.com>
2015-04-27 22:48:31 +0300

    Update autostyle.js

--- autostyle.js
+++ autostyle.js
@@ -1,9 +1,9 @@
-var fileref=document.createElement('link');
-fileref.setAttribute('rel','stylesheet');
-fileref.setAttribute('type','text/css');
-fileref.setAttribute('href','/3ds/web.css');
-if(navigator.platform.indexOf('Nintendo 3DS')>=0){
-	fileref.setAttribute('href','/3ds/3ds.css');
-	window.setInterval(function(){window.scrollTo(40,220);},50);
+var fileref=document.createElement('link')
+fileref.setAttribute('rel', 'stylesheet')
+fileref.setAttribute('type', 'text/css')
+fileref.setAttribute('href', '/3ds/web.css')
+if (navigator.platform.indexOf('Nintendo 3DS') > -1){
+    fileref.setAttribute('href', '/3ds/3ds.css')
+    window.setInterval(function(){window.scrollTo(40,220);},50);
 }
-document.getElementsByTagName('head')[0].appendChild(fileref);
+document.getElementsByTagName('head')[0].appendChild(fileref)
