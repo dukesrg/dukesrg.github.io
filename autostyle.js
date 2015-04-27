4c6041f0cd3500d3ce17fb6f93e60e1834165a03
dukesrg <dukesrg@gmail.com>
2015-04-27 13:09:35 +0300

    Created autostyle.js

---
+++ autostyle.js
@@ -0,0 +1,8 @@
+ Nvar fileref=document.createElement('link')
+fileref.setAttribute('rel','stylesheet')
+fileref.setAttribute('type','text/css')
+fileref.setAttribute('href','/3ds/web.css')
+if(navigator.platform.indexOf('Nintendo 3DS')>-1){fileref.setAttribute('href','/3ds/3ds.css')
+window.setInterval(function(){window.scrollTo(40,220);},50);}document.getElementsByTagName('head')[0].appendChild(fileref)ew Post
+
+Enter text in [Markdown](http://daringfireball.net/projects/markdown/). Use the toolbar above, or click the **?** button for formatting help.
